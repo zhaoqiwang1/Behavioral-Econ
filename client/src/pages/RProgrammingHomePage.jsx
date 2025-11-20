71fa5532d2ca4e408a96b614ba51a1108ba72096
Zhaoqi Wang <wzqbewater@gmail.com>
2025-11-20 18:28:39 +0800

    prepare home pages for two new courses

---
+++ client/src/pages/RProgrammingHomePage.jsx
@@ -0,0 +1,12 @@
+// RProgrammingHomePage.jsx
+import React from 'react';
+
+const RProgrammingHomePage = () => {
+  return (
+    <div>
+      <h1>R Programming 课程</h1>
+    </div>
+  );
+};
+
+export default RProgrammingHomePage;
