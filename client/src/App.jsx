23c730cf7c567d2d9e36d5b5d79b9bd48574044a
Zhaoqi Wang <wzqbewater@gmail.com>
2025-11-20 21:29:07 +0800

    make sure footer is always at the bottom of all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,69 +37,71 @@ function App() {
         <div className="App">
           <BrowserCloseHandler />
           <Toaster position="top-center" />
-          <Routes>
-            {/* HomePages path */}
-            <Route path="/" element={<MainPortalPage />} />
-            <Route path="/courses/behavioral-science" element={<BehavioralScienceHomePage />} />
-            <Route path="/courses/r-programming" element={<RProgrammingHomePage />} />
-            <Route path="/courses/logic" element={<LogicHomePage />} />
-            {/* Auth Page path */}
-            <Route path="/auth/login" element={<Login />} />
-            <Route path="/auth/register" element={<Register />} />
-            <Route path="/auth/settings" element={
-              <ProtectedRoute>
-                <UserSettings />
-              </ProtectedRoute>
-              } />
-            {/* Games Page path */}
-            <Route path="/games/risk-attitude-elicit" element={
-              <ProtectedRoute>
-                <RiskAttiElicit />
-              </ProtectedRoute>
-              } />
-            <Route path="/games/ambiguity-attitude-elicit" element={
-              <ProtectedRoute>
-                <AmbiguityAttiElicit />
-              </ProtectedRoute>
-              } />
-            <Route path="/games/public-goods" element={
-              <ProtectedRoute>
-                <PublicGoodsGame />
-              </ProtectedRoute>
-              } />
-            <Route path="/games/overconfidence-game" element={
-              <ProtectedRoute>
-                <OverconfidenceGame />
-              </ProtectedRoute>
-              } />
-            <Route path="/games/mbti-elicit-game" element={
-              <ProtectedRoute>
-                <MBTIElicit />
-              </ProtectedRoute>
-              } />
-            <Route path="/games/game-under-ambiguity" element={
-              <ProtectedRoute>
-                <GameUnderAmbiguity />
-              </ProtectedRoute>
-              } />
-            <Route path="/games/confirmation-bias-game" element={
-              <ProtectedRoute>
-                <ConfirmationBiasGame />
-              </ProtectedRoute>
-              } />
-            <Route path="/games/ous-survey" element={
-              <ProtectedRoute>
-                <OUSSurvey />
-              </ProtectedRoute>
-              } />
-            {/* Game Results Page path */}
-            <Route path="/gameresults/show-risk-attitudes" element={
-              <ProtectedRoute>
-                <ShowRiskAtti />
-              </ProtectedRoute>
-              } />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
+          <div className="RoutesClass">
+            <Routes>
+              {/* HomePages path */}
+              <Route path="/" element={<MainPortalPage />} />
+              <Route path="/courses/behavioral-science" element={<BehavioralScienceHomePage />} />
+              <Route path="/courses/r-programming" element={<RProgrammingHomePage />} />
+              <Route path="/courses/logic" element={<LogicHomePage />} />
+              {/* Auth Page path */}
+              <Route path="/auth/login" element={<Login />} />
+              <Route path="/auth/register" element={<Register />} />
+              <Route path="/auth/settings" element={
+                <ProtectedRoute>
+                  <UserSettings />
+                </ProtectedRoute>
+                } />
+              {/* Games Page path */}
+              <Route path="/games/risk-attitude-elicit" element={
+                <ProtectedRoute>
+                  <RiskAttiElicit />
+                </ProtectedRoute>
+                } />
+              <Route path="/games/ambiguity-attitude-elicit" element={
+                <ProtectedRoute>
+                  <AmbiguityAttiElicit />
+                </ProtectedRoute>
+                } />
+              <Route path="/games/public-goods" element={
+                <ProtectedRoute>
+                  <PublicGoodsGame />
+                </ProtectedRoute>
+                } />
+              <Route path="/games/overconfidence-game" element={
+                <ProtectedRoute>
+                  <OverconfidenceGame />
+                </ProtectedRoute>
+                } />
+              <Route path="/games/mbti-elicit-game" element={
+                <ProtectedRoute>
+                  <MBTIElicit />
+                </ProtectedRoute>
+                } />
+              <Route path="/games/game-under-ambiguity" element={
+                <ProtectedRoute>
+                  <GameUnderAmbiguity />
+                </ProtectedRoute>
+                } />
+              <Route path="/games/confirmation-bias-game" element={
+                <ProtectedRoute>
+                  <ConfirmationBiasGame />
+                </ProtectedRoute>
+                } />
+              <Route path="/games/ous-survey" element={
+                <ProtectedRoute>
+                  <OUSSurvey />
+                </ProtectedRoute>
+                } />
+              {/* Game Results Page path */}
+              <Route path="/gameresults/show-risk-attitudes" element={
+                <ProtectedRoute>
+                  <ShowRiskAtti />
+                </ProtectedRoute>
+                } />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </div>
           <Footer />
         </div> 
       </AuthProvider>
