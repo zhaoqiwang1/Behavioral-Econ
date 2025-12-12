458d0cb49a415dc79715cc42981f5eac891d9fec
Zhaoqi Wang <wzqbewater@gmail.com>
2025-12-12 14:24:30 +0800

    try new things for loading r markdown

--- client/src/pages/RProgrammingHomePage.jsx
+++ client/src/pages/RProgrammingHomePage.jsx
@@ -9,6 +9,10 @@ const RProgrammingHomePage = () => {
        <div>
         <h1>R语言课程正在开发中...</h1>
        </div>
+        <p>
+          <a href="https://zhaoqiwang1.github.io/Basics_Part1/Basics_1.html" target="_blank" rel="noopener noreferrer">Basics_Part1（HTML版）</a>
+          <a href="https://zhaoqiwang1.github.io/Basics_Part1/Basics_1.pdf" target="_blank" rel="noopener noreferrer" download>下载 PDF 版</a>
+        </p>
     </div>
   );
 };
