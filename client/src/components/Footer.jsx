67c450e1e6953eba3158b7c9f24e336ddadc9527
Zhaoqi Wang <wzqbewater@gmail.com>
2026-03-23 12:07:03 +0800

    footer update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,10 +4,13 @@ import styles from './Footer.module.css'; // 可选的 CSS 模块
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>Copyright © 2025 王兆祺的教学平台 - 致力于为学生提供优质的学习体验 版权所有 
+      <p>Copyright © 2026 王兆祺的教学平台 - 致力于为学生提供优质的学习体验 版权所有 
 
        <a href="mailto:wzqbewater@gmail.com">
         <span> 联系方式 </span>
+       </a>
+      <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">
+        粤ICP备2026027916号-1
        </a>
       </p>
     </footer>
