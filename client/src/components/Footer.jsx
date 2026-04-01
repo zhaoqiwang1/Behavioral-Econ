05b391062de9b98c85b9218fd4a0066a689b2533
Zhaoqi Wang <wzqbewater@gmail.com>
2026-04-01 13:43:04 +0800

    polic record change

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,12 +6,24 @@ const Footer = () => {
     <footer className={styles.footer}>
       <p>Copyright © 2026 王兆祺的教学平台 - 致力于为学生提供优质的学习体验 版权所有 
 
-       <a href="mailto:wzqbewater@gmail.com">
-        <span> 联系方式 </span>
-       </a>
+      <a href="mailto:wzqbewater@gmail.com">
+       <span> 联系方式 </span>
+      </a>
+      <br />
       <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">
         粤ICP备2026027916号-1
-       </a>
+      </a>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <img
+        src="/assets/images/portalPage/police_logo.png"
+        width="17"
+        style={{ position: "relative", top: "3px" }} 
+        alt="公安备案"
+      />
+      &nbsp;
+      <a href="https://beian.mps.gov.cn/#/query/webSearch" target="_blank" rel="noopener noreferrer">
+      粤公网安备44011302005363号
+      </a>
       </p>
     </footer>
   );
